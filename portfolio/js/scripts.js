0d8530930ae47494a8c72401520d4336874cabeb
Jason Campbell <43692594+jcampbell18@users.noreply.github.com>
2019-03-04 07:40:08 -0800

    Updated scripts.js

---
+++ portfolio/js/scripts.js
@@ -0,0 +1,15 @@
+// Everything OK?
+console.log('OK');
+
+// Your Awesome Scripts
+
+
+// lightGallery
+$('#lightgallery').lightGallery({
+    thumbnail: true,
+    fullScreen: true,
+    animateThumb: false,
+    showThumbByDefault: false,
+    zoom: false,
+    scale: 1
+});
