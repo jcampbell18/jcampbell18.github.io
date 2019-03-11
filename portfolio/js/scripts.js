70878d5831800d8c5a70d430be0808f72a3299dd
Jason C <jcampbell18@eagles.ewu.edu>
2019-03-11 11:36:26 -0700

    add fonts
    LightGallery fonts

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -11,15 +11,19 @@ $('#lightgallery').lightGallery({
     animateThumb: false,
     showThumbByDefault: false,
     zoom: false,
+    download: false,
     scale: 1
 });
 
 //for index.html
-// $('.gallery').lightGallery({
-//     thumbnail: true,
-//     fullScreen: true,
-//     animateThumb: false,
-//     showThumbByDefault: false,
-//     zoom: false,
-//     scale: 1
-// });
+$('.gallery').lightGallery({
+    thumbnail: true,
+    fullScreen: true,
+    animateThumb: false,
+    showThumbByDefault: false,
+    zoom: false,
+    download: false,
+    mode: 'lg-zoom-in-big',
+    speed: 400,
+    scale: 1
+});
