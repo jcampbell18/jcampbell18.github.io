87bc9175bd9abea61216703814c1270487c33d84
Jason Campbell <43692594+jcampbell18@users.noreply.github.com>
2019-03-24 00:48:54 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,32 @@ console.log('OK');
 // Output Current Year
 $('.currentYear').text( (new Date).getFullYear() );
 
+//animateCSS
+$('#JC').animateCSS('bounceInDown', function() {
+    delay: 500,
+
+    $(this).animateCSS('flip', {
+        delay: 2000
+    });
+
+});
+
+$('#nav-link1').hover( function() {
+    $(this).animateCSS('heartBeat');
+});
+
+$('#nav-link2').hover( function() {
+    $(this).animateCSS('heartBeat');
+});
+
+$('#nav-link3').hover( function() {
+    $(this).animateCSS('heartBeat');
+});
+
+$('#nav-link4').hover( function() {
+    $(this).animateCSS('heartBeat');
+});
+
 
 // lightGallery
 $('#lightgallery').lightGallery({
